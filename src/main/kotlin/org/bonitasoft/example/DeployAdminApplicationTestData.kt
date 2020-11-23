37666c4bbc2972eb65101ae2679032f7ebba0d7d
Baptiste Mesta <baptiste.mesta@gmail.com>
2020-11-23 17:06:07 +0100

    add rest api extensions

--- src/main/kotlin/org/bonitasoft/example/DeployAdminApplicationTestData.kt
+++ src/main/kotlin/org/bonitasoft/example/DeployAdminApplicationTestData.kt
@@ -9,29 +9,29 @@ import java.util.function.Consumer
 class DeployAdminApplicationTestData : Consumer<APIClient> {
     override fun accept(apiClient: APIClient) {
         val faker = Faker()
-
-        DeployEmployeeBDM().deploy(apiClient)
-//        SetupOrganization().accept(apiClient)
-        val calledProcess = BigData(100).apply {
-            deploy(apiClient)
-        }
-        StartXProcessesWithData(calledProcess.name, calledProcess.version, 1).deploy(apiClient)
-        val formName = "custompage_${faker.dog().breed()}"
-        GeneratedPage(formName, "form").deploy(apiClient)
-        (1..200).forEach {
-            GeneratedProcessWithForms("${faker.animal().name()}-$it", formName).deploy(apiClient)
-        }
-        (1..40).forEach {
-            ProcessNotEnabled("${faker.rickAndMorty().character()}-$it").deploy(apiClient)
-        }
-        (1..60).forEach {
-            ProcessHavingConfigurationIssues("${faker.dune().planet()}-$it").deploy(apiClient)
-        }
-        (1..500).forEach {
-            GeneratedPage("custompage_${faker.dog().breed()}$it", "form").deploy(apiClient)
-        }
+//
+//        DeployEmployeeBDM().deploy(apiClient)
+////        SetupOrganization().accept(apiClient)
+//        val calledProcess = BigData(100).apply {
+//            deploy(apiClient)
+//        }
+//        StartXProcessesWithData(calledProcess.name, calledProcess.version, 1).deploy(apiClient)
+//        val formName = "custompage_${faker.dog().breed()}"
+//        GeneratedPage(formName, "form").deploy(apiClient)
+//        (1..200).forEach {
+//            GeneratedProcessWithForms("${faker.animal().name()}-$it", formName).deploy(apiClient)
+//        }
+//        (1..40).forEach {
+//            ProcessNotEnabled("${faker.rickAndMorty().character()}-$it").deploy(apiClient)
+//        }
+//        (1..60).forEach {
+//            ProcessHavingConfigurationIssues("${faker.dune().planet()}-$it").deploy(apiClient)
+//        }
+//        (1..500).forEach {
+//            GeneratedPage("custompage_${faker.dog().breed()}$it", "form").deploy(apiClient)
+//        }
         (1..50).forEach {
-            GeneratedPage("custompage_${faker.dog().breed()}$it", "apiExtension").deploy(apiClient)
+            GeneratedRestApiExt("custompage_${faker.dog().name().replace(" ", "")}$it").deploy(apiClient)
         }
 
 
